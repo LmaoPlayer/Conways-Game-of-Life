f2b26e8b73df774ce4d5c70274e7997ba6fd1987
BuildTools <unconfigured@null.spigotmc.org>
2024-03-15 16:20:55 +0100

    fixed

--- Form1.cs
+++ Form1.cs
@@ -14,8 +14,10 @@ namespace WindowsFormsApp1
 {
     public partial class Form1 : Form
     {
+        int TickRate = 500;
         PictureBox Field = new PictureBox();
         string Cool;
+        int FieldSize = 10;
         int col = 100;
         int row = 70;
         int[,] FriendList;
@@ -28,7 +30,7 @@ namespace WindowsFormsApp1
             InitializeComponent();
             CreateField();
             Read();
-/*            RunTheGame();*/
+            RunTheGame();
         }
         private void Form1_Load(object sender, EventArgs e)
         {
@@ -38,7 +40,7 @@ namespace WindowsFormsApp1
         {
             
 
-            Field.Size = new Size(col*10, row*10);
+            Field.Size = new Size(col*FieldSize, row*FieldSize);
             Field.BorderStyle = BorderStyle.FixedSingle;
             Field.Location = new Point(5, 5);
             Field.Paint += new PaintEventHandler(DrawTheCells);
@@ -52,7 +54,7 @@ namespace WindowsFormsApp1
             {
                 for (int j = 0; j < row; j++)
                 {
-                    GridCounting[i, j] = true;
+                    GridCounting[i, j] = false;
                     FriendList[i, j] = 0;
                 }
             }
@@ -71,9 +73,7 @@ namespace WindowsFormsApp1
                     {
                         if (Line[Column] == 'O')
                         {
-                            GridCounting[RepRow, Column] = true;
-                            CoordsX.Add(Column);
-                            CoordsY.Add(RepRow);
+                            GridCounting[Column, RepRow] = true;
                         }
                     }
                 }
@@ -83,19 +83,37 @@ namespace WindowsFormsApp1
         private void DrawTheCells(object sender, PaintEventArgs e)
         {
             Graphics g = e.Graphics;
-            Pen pen = new Pen(Color.Black, 10);
-            for (int i = 0; i<CoordsY.Count; i++)
+            SolidBrush ImaBrush = new SolidBrush(Color.Black);
+            for (int i = 0; i<col; i++)
             {
-                for (int j = 0; j < CoordsX.Count; j++)
+                for (int j = 0; j < row; j++)
                 {
-                    g.DrawRectangle(pen, new Rectangle(new Point(CoordsX[j]*10+5, CoordsY[i]*10+5), new Size(10, 10)));
+                    if (GridCounting[i, j])
+                    {
+                        g.FillRectangle(ImaBrush, new Rectangle(new Point(i * FieldSize, j * FieldSize), new Size(FieldSize, FieldSize)));
+                    }
+                    
                 }
             }
             
         }
         private async void RunTheGame()
         {
-            await Task.Delay(1);
+            while (true)
+            {
+                await UpdateTheCells();
+
+                Application.DoEvents();
+
+                await Task.Delay(16);
+            }
+
+        }
+        private async Task UpdateTheCells()
+        {
+            NextStepPlease();
+
+            await Task.Delay(1000);
         }
         private void CheckTheFriends()
         {
@@ -108,17 +126,88 @@ namespace WindowsFormsApp1
                     int pCol = kol == 0 ? col - 1 : kol - 1;
                     int nCol = kol == col - 1 ? 0 : kol + 1;
                     int nRow = rij == row - 1 ? 0 : rij + 1;
-                    if (!GridCounting[pCol, pRow]) AmmountOfFriends++;
-                    if (!GridCounting[pCol, nRow]) AmmountOfFriends++;
-                    if (!GridCounting[pCol, rij]) AmmountOfFriends++;
-                    if (!GridCounting[nCol, pRow]) AmmountOfFriends++;
-                    if (!GridCounting[nCol, nRow]) AmmountOfFriends++;
-                    if (!GridCounting[nCol, rij]) AmmountOfFriends++;
-                    if (!GridCounting[kol, pRow]) AmmountOfFriends++;
-                    if (!GridCounting[kol, nRow]) AmmountOfFriends++;
+                    if (GridCounting[pCol, pRow]) AmmountOfFriends++;
+                    if (GridCounting[pCol, nRow]) AmmountOfFriends++;
+                    if (GridCounting[pCol, rij]) AmmountOfFriends++;
+                    if (GridCounting[nCol, pRow]) AmmountOfFriends++;
+                    if (GridCounting[nCol, nRow]) AmmountOfFriends++;
+                    if (GridCounting[nCol, rij]) AmmountOfFriends++;
+                    if (GridCounting[kol, pRow]) AmmountOfFriends++;
+                    if (GridCounting[kol, nRow]) AmmountOfFriends++;
                     FriendList[kol, rij] = AmmountOfFriends;
                 }
             }
         }
-    }
+        private void NextStepPlease()
+        {
+            CheckTheFriends();
+            for (int Columns = 0; Columns < col; Columns++)
+            {
+                for (int Rowing = 0; Rowing < row; Rowing++)
+                {
+                    //MessageBox.Show(Grid[Columns, Rowing].Name + " has " + FriendList[Columns, Rowing].ToString() + " friends.");
+                    if (FriendList[Columns, Rowing] < 2 || FriendList[Columns, Rowing] > 3)
+                    {
+
+                        GridCounting[Columns, Rowing] = false;
+                    }
+                    else if (FriendList[Columns, Rowing] == 3)
+                    {
+                        GridCounting[Columns, Rowing] = true;
+                    }
+                }
+            }
+            Field.Invalidate();
+        }
+        //new
+/*        private void SpeedingGoBrr(object sender, EventArgs e)
+        {
+            if (!SpeedTheShitUp)
+            {
+                TickRate = 250;
+                SpeedTheShitUp = true;
+                SpeedUp.Text = "Slow down";
+            }
+            else
+            {
+                TickRate = 500;
+                SpeedTheShitUp = false;
+                SpeedUp.Text = "Speed up";
+            }
+        }
+        private void ResetIt(object sender, EventArgs e)
+        {
+            Button HowWillIReset = (Button)sender;
+
+            StartedTheGame = false;
+            EndTheGame.Visible = false;
+            EndTheGame.Enabled = false;
+            StartTheGame.Visible = true;
+            StartTheGame.Enabled = true;
+            for (int i = 0; i < col; i++)
+            {
+                for (int j = 0; j < row; j++)
+                {
+                    GridCounting[i, j] = true;
+                    if (HowWillIReset.Text == "Reset")
+                    {
+                        Grid[i, j].BackColor = Color.Transparent;
+                    }
+                    else
+                    {
+                        Controls.Remove(Grid[i, j]);
+                    }
+
+                }
+            }
+        }
+        private void EndIt(object sender, EventArgs e)
+        {
+            StartedTheGame = false;
+            EndTheGame.Visible = false;
+            EndTheGame.Enabled = false;
+            StartTheGame.Visible = true;
+            StartTheGame.Enabled = true;
+        }
+    }*/
 }
